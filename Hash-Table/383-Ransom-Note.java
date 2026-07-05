cf6f5b02a54d5890281f1d678128c2835932d7cc
Kunal Arela <arelakunal238@gmail.com>
2026-07-06 01:37:27 +0530

    Add 383. Ransom Note

---
+++ Hash-Table/383-Ransom-Note.java
@@ -0,0 +1,24 @@
+// 383. Ransom Note (Easy)
+// https://leetcode.com/problems/ransom-note/
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < magazine.length(); i++) {
+            freq[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int index = ransomNote.charAt(i) - 'a';
+
+            if (freq[index] == 0) {
+                return false;
+            }
+
+            freq[index]--;
+        }
+
+        return true;
+    }
+}
