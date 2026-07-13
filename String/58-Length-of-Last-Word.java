1797b4ff78520955e2fca4a651e8a7a69f1a2904
Kunal Arela <arelakunal238@gmail.com>
2026-07-13 23:35:43 +0530

    Add 58. Length of Last Word

---
+++ String/58-Length-of-Last-Word.java
@@ -0,0 +1,8 @@
+// 58. Length of Last Word (Easy)
+// https://leetcode.com/problems/length-of-last-word/
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+       String [] ch = new String(s);
+    }
+}
