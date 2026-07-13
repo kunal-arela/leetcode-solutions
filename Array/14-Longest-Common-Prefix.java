d5b0d3ae18026ea88da7474ac968ea9da63dcbc9
Kunal Arela <arelakunal238@gmail.com>
2026-07-14 00:46:44 +0530

    Add 14. Longest Common Prefix

---
+++ Array/14-Longest-Common-Prefix.java
@@ -0,0 +1,19 @@
+// 14. Longest Common Prefix (Easy)
+// https://leetcode.com/problems/longest-common-prefix/
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for(int i = 1;i<strs.length;i++){
+            while(!strs[i].startsWith(prefix)){
+              prefix = prefix.substring(0 , prefix.length() - 1);
+            }
+            if(prefix.isEmpty()){
+              return "";
+            }
+
+            
+        }
+        return prefix;
+    }
+}
