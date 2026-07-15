3f3a772ea8c002b5c263b5f3a2fd81d2ab94e2b6
Kunal Arela <arelakunal238@gmail.com>
2026-07-15 22:35:54 +0530

    Update 3658. GCD of Odd and Even Sums

--- Math/3658-GCD-of-Odd-and-Even-Sums.java
+++ Math/3658-GCD-of-Odd-and-Even-Sums.java
@@ -3,6 +3,33 @@
 
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        return n;
+        if(n == 1){
+            return n;
+        }
+        int sumodd = 0;
+       int evensum = 0;
+       int count = 0;
+       int i = 0; 
+       int a = 0;
+      while(count < n*2){
+       if(i % 2 != 0){
+        sumodd += i;
+         count++;
+       }else if(i % 2 == 0){
+        evensum += i;
+        count++;
+       }
+        i++;
+      }
+      int m = Math.min(sumodd , evensum);
+      int p = Math.max(sumodd , evensum);
+      for(int j= m;j>0;j--){
+        if(m % j == 0 && p % j == 0){
+           a = j;
+           break;
+        }
+      }
+      return a;
+     
     }
 }
