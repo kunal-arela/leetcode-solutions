c27d9649dfe1bcb9d3276812058b009668c50788
Kunal Arela <arelakunal238@gmail.com>
2026-07-15 21:42:35 +0530

    Add 3658. GCD of Odd and Even Sums

---
+++ Math/3658-GCD-of-Odd-and-Even-Sums.java
@@ -0,0 +1,8 @@
+// 3658. GCD of Odd and Even Sums (Easy)
+// https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
