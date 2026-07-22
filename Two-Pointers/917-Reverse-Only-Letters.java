17427d691cf2f61c0931c648af142a47da1fe4cb
Kunal Arela <arelakunal238@gmail.com>
2026-07-23 00:32:24 +0530

    Add 917. Reverse Only Letters

---
+++ Two-Pointers/917-Reverse-Only-Letters.java
@@ -0,0 +1,30 @@
+// 917. Reverse Only Letters (Easy)
+// https://leetcode.com/problems/reverse-only-letters/
+
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        char[] arr = s.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetter(arr[left])) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetter(arr[right])) {
+                right--;
+            }
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+}
