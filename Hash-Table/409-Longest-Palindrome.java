da4cbdbbe0cb39af7de11743612563fde8372798
Kunal Arela <arelakunal238@gmail.com>
2026-07-24 00:55:37 +0530

    Update 409. Longest Palindrome

--- Hash-Table/409-Longest-Palindrome.java
+++ Hash-Table/409-Longest-Palindrome.java
@@ -2,26 +2,19 @@
 // https://leetcode.com/problems/longest-palindrome/
 
 class Solution {
-    public int longestPalindrome(String s) {
-        int[] count = new int[128];
-        for (char c : s.toCharArray()) {
-            count[c]++;
+    public int firstUniqChar(String s) {
+        int [] arr = new int[26];
+        char [] ch = s.toCharArray();
+        for(int i = 0;i<ch.length;i++){
+            arr[ch[i] - 'a']++;    
         }
-
-        int length = 0;
-        boolean hasOdd = false;
-
-        for (int c : count) {
-            // Add the largest even part of the frequency
-            length += (c / 2) * 2;
-            
-            // Check if there is at least one odd frequency character available
-            if (c % 2 != 0) {
-                hasOdd = true;
+         for(int i = 0;i<ch.length;i++){
+             if(arr[ch[i] - 'a'] == 1){
+                return i;
+               
             }
+           
         }
-
-        // If at least one character had an odd frequency, we can use one in the center
-        return hasOdd ? length + 1 : length;
+        return -1;
     }
 }
