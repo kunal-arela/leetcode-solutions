102b1c7511aaab734c56feabfb380ef2229ff9aa
Kunal Arela <arelakunal238@gmail.com>
2026-07-24 01:00:17 +0530

    Add 1025. Divisor Game

---
+++ Math/1025-Divisor-Game.java
@@ -0,0 +1,11 @@
+// 1025. Divisor Game (Easy)
+// https://leetcode.com/problems/divisor-game/
+
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n % 2 != 0){
+            return false;
+        }
+        return true;
+    }
+}
