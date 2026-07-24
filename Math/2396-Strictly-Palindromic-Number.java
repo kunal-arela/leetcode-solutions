fdeb799dd92a0c9ded18008ba7d27d74f4742dca
Kunal Arela <arelakunal238@gmail.com>
2026-07-25 00:38:23 +0530

    Add 2396. Strictly Palindromic Number

---
+++ Math/2396-Strictly-Palindromic-Number.java
@@ -0,0 +1,8 @@
+// 2396. Strictly Palindromic Number (Medium)
+// https://leetcode.com/problems/strictly-palindromic-number/
+
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+}
