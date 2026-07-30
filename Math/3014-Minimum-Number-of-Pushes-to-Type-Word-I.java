b1d6e4ef1e15dc767937d53b32e27cb9abe03058
Kunal Arela <arelakunal238@gmail.com>
2026-07-31 00:39:45 +0530

    Add 3014. Minimum Number of Pushes to Type Word I

---
+++ Math/3014-Minimum-Number-of-Pushes-to-Type-Word-I.java
@@ -0,0 +1,14 @@
+// 3014. Minimum Number of Pushes to Type Word I (Easy)
+// https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i/
+
+class Solution {
+    public int minimumPushes(String word) {
+        int ans = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            ans += i / 8 + 1;
+        }
+
+        return ans;
+    }
+}
