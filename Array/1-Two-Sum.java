9cc365c333f8da3349bb192528710a33d9c29358
Kunal Arela <arelakunal238@gmail.com>
2026-08-02 02:05:16 +0530

    Add 1. Two Sum

---
+++ Array/1-Two-Sum.java
@@ -0,0 +1,16 @@
+// 1. Two Sum (Easy)
+// https://leetcode.com/problems/two-sum/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int [] arr = new int[2];
+        for(int i = 0;i<nums.length;i++){
+            for(int j = 0;j<nums.length;j++)
+            if(nums[i] + nums[j] == target){
+                arr[0] = i;
+                arr[1] = j;
+             }
+        }
+        return arr;
+    }
+}
