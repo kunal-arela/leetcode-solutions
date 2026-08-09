0b5807d8269a7762e6895f1c1593873cd16a12e3
Kunal Arela <arelakunal238@gmail.com>
2026-08-09 23:03:36 +0530

    Update 1652. Defuse the Bomb

--- Array/1652-Defuse-the-Bomb.java
+++ Array/1652-Defuse-the-Bomb.java
@@ -4,27 +4,26 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
         int n = code.length;
-        int[] arr = new int[n];
-        if (k == 0) {
+        int [] arr = new int[n];
+        if(k == 0){
             return arr;
-        }
-        if (k > 0) {
-            for (int i = 0; i < n; i++) {
-                int sum = 0;
-                for (int j = 1; j <= k; j++) {
-                    sum += code[(i + j) % n];
+        }else if(k > 0){
+            for(int i = 0;i<n;i++){
+                int sum  = 0;
+                for(int j = 1;j<=k;j++){
+                    sum += code[(i+j) % n];
                 }
                 arr[i] = sum;
             }
-        }
-        else { 
-            for (int i = 0; i < n; i++) {
-                int sum = 0;
-                for (int j = 1; j <= -k; j++) {
-                    sum += code[(i - j + n) % n];
+        }else{
+            for(int i = 0;i<n;i++){
+                int sum  = 0;
+                for(int j = 1;j<=-k;j++){
+                    sum += code[(i - j + n)  % n];
                 }
                 arr[i] = sum;
             }
+            
         }
         return arr;
     }
