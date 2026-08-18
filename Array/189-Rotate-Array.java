c7b9d1884e82cad6e56487403ed8476269a31cc7
Kunal Arela <arelakunal238@gmail.com>
2026-08-18 23:22:10 +0530

    Add 189. Rotate Array

---
+++ Array/189-Rotate-Array.java
@@ -0,0 +1,10 @@
+// 189. Rotate Array (Medium)
+// https://leetcode.com/problems/rotate-array/
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int [] arr = new int[k];
+        
+    }
+}
