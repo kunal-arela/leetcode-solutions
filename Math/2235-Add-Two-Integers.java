a09e38106e8a6b16b3a3d85d582ca35efe77bdd0
Kunal Arela <arelakunal238@gmail.com>
2026-08-20 22:39:31 +0530

    Add 2235. Add Two Integers

---
+++ Math/2235-Add-Two-Integers.java
@@ -0,0 +1,8 @@
+// 2235. Add Two Integers (Easy)
+// https://leetcode.com/problems/add-two-integers/
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
