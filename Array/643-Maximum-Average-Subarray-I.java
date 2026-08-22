a67a0329c6b0e9f01cbaabe1a7a8a2d481ce00ac
Kunal Arela <arelakunal238@gmail.com>
2026-08-23 01:00:02 +0530

    Update 643. Maximum Average Subarray I

--- Array/643-Maximum-Average-Subarray-I.java
+++ Array/643-Maximum-Average-Subarray-I.java
@@ -3,16 +3,18 @@
 
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int n = nums.length;
-        double Max = 0;
-        double sum = 0;
-        for(int i = 0;i<k;i++){
-            sum += nums[i];
-           double avg = sum/k;
-            Max = Math.max(Max  , avg );
-             sum -= nums[i];
-
-        }
-        return Max;
+       int n = nums.length;
+       double sum = 0;
+       for(int i = 0;i<k;i++){
+        sum += nums[i];
+       }
+       double Max = sum;
+       for(int i = k;i<n;i++){
+        sum += nums[i];
+        sum -= nums[i - k];
+        Max = Math.max(sum , Max);
+       }
+       double avg = Max/k;
+       return avg;
     }
 }
