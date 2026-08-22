d268f0e8b74b9aa67612e0d6d90973b9303d527f
Kunal Arela <arelakunal238@gmail.com>
2026-08-23 01:26:37 +0530

    Add 209. Minimum Size Subarray Sum

---
+++ Array/209-Minimum-Size-Subarray-Sum.java
@@ -0,0 +1,18 @@
+// 209. Minimum Size Subarray Sum (Medium)
+// https://leetcode.com/problems/minimum-size-subarray-sum/
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int sum = 0;
+        for(int i = right;i<n;i++){
+         sum += nums[i];
+         if(sum > )
+        }
+        while(sum >= target){
+            
+        }
+        return 0;
+    }
+}
