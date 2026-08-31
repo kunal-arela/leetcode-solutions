7c33cdad6dd58c374a8e459427582c58dae8d508
Kunal Arela <arelakunal238@gmail.com>
2026-08-31 17:37:48 +0530

    Add 202. Happy Number

---
+++ Hash-Table/202-Happy-Number.java
@@ -0,0 +1,20 @@
+// 202. Happy Number (Easy)
+// https://leetcode.com/problems/happy-number/
+
+class Solution {
+    public boolean isHappy(int n) {
+        while (n != 1 && n != 4) {
+            int sum = 0;
+            while (n != 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n = n / 10;
+            }
+            n = sum;
+        }
+        if (n == 1) {
+            return true;
+        }
+        return false;
+    }
+}
