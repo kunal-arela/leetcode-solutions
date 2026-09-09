8fedfae2fe6df4e5c361627e2c6550a652d39dde
Kunal Arela <arelakunal238@gmail.com>
2026-09-10 00:45:15 +0530

    Update 3871. Count Commas in Range II

--- Math/3871-Count-Commas-in-Range-II.java
+++ Math/3871-Count-Commas-in-Range-II.java
@@ -3,21 +3,12 @@
 
 class Solution {
     public long countCommas(long n) {
-
         long ans = 0;
         long start = 1000;
-        long commas = 1;
-
         while (start <= n) {
-
-            long end = Math.min(n, start * 1000 - 1);
-
-            ans += (end - start + 1) * commas;
-
+            ans += n - start + 1;
             start *= 1000;
-            commas++;
         }
-
         return ans;
     }
 }
